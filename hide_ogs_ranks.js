0e46eb251401b6e34a3f1454368aa45fcc14a2ba
stefan.koegel <stefan.koegel@uni-ulm.de>
2019-11-29 22:13:56 +0100

    move most of the hiding logic into css; also hide rank info on user page

--- hide_ogs_ranks.js
+++ hide_ogs_ranks.js
@@ -1,40 +1,69 @@
 
-const rankRegex = /\[.*\]$/;
-const nameClass = "player-name";
-const rankClass = "Player-rank";
-const spanSelector = `span.${nameClass}, span.${rankClass}`;
+// We hide most of the ratings via CSS.
+// This also includes the rating table and graph on the user pages.
+const cssStyle = `
+  span.Player-rank { display: none }
+  span.rating      { display: none }
+  span.rank        { display: none }
+  div.ratings-container       { filter: blur(20px) }
+  div.ratings-graph           { filter: blur(20px) }
+  .avatar-container span.Player-rank { filter: blur(10px); display: inline }`
 
-function hideRankInChildren(node) {
-  const spans = Array.from(node.querySelectorAll(spanSelector));
-  spans.forEach(hideRankInSpan);
-}
+const styleElement = document.createElement("style")
+styleElement.type = "text/css"
+styleElement.appendChild(document.createTextNode(cssStyle));
+document.head.append(styleElement);
 
-function hideRankInSpan(span) {
-  if(span.textContent.match(rankRegex)) {
-    span.textContent = span.textContent.replace(rankRegex, "");
-  }
-}
-
-// Hide ranks that are in the document when the extension is executed
-hideRankInChildren(document.body);
-
-function hideRanksOnMutation(mutations) {
-  for(let mutation of mutations) {
-    const target = mutation.target
-    if(target.tagName == "SPAN") {
-      const classList = target.classList;
-      if(classList.contains(nameClass) || classList.contains(rankClass)) {
-        hideRankInSpan(target);
-      }
-    } else {
-      hideRankInChildren(target);
+// This function removes ranks from user names via regular expressions.
+function hideRanks(node) {
+  const names = node.querySelectorAll("span.player-name");
+  for(const name of names) {
+    const text = name.textContent;
+    if(text.match(/\[.*\]$/)) {
+      name.textContent = text.replace(/\[.*\]$/, "");
     }
   }
 }
 
-const observer = new MutationObserver(hideRanksOnMutation);
+// Hide ranks in user names that are on the website when the extension is executed
+hideRanks(document.body);
 
+// We use a mutation observer to listen for the addition of new user names after the website has loaded
 const config = { childList: true, subtree: true, characterData: false, attributes: false };
+const spanObserver = new MutationObserver(hideRanksOnMutation);
+spanObserver.observe(document.body, config);
 
-// Hide ranks that are added later on via Web API
-observer.observe(document.body, config);
+function hideRanksOnMutation(mutations) {
+  for(let mutation of mutations) {
+    for(let added of mutation.addedNodes) {
+      hideRanks(added.parentNode);
+    }
+  }
+}
+
+// Try to add a click listener to the blurred graph and table on the user page.
+// If a click is registered, remove the blur.
+function addUnhideRatingsListener() {
+  const tableContainer = document.querySelector("div.ratings-container");
+  const graphContainer = document.querySelector("div.ratings-graph");
+  const playerRank = document.querySelector(".avatar-container span.Player-rank");
+
+  if(tableContainer && graphContainer && playerRank) {
+    tableContainer.onclick = cleanUp;
+    graphContainer.onclick = cleanUp;
+
+    function cleanUp(event) {
+      tableContainer.style.setProperty("filter", "none");
+      graphContainer.style.setProperty("filter", "none");
+      playerRank.style.setProperty("filter", "none");
+
+      event.stopPropagation();
+      tableContainer.onclick = null;
+      graphContainer.onclick = null;
+      window.setTimeout(addUnhideRatingsListener, 1000);
+    }
+  } else {
+      window.setTimeout(addUnhideRatingsListener, 1000);
+  }
+}
+window.setTimeout(addUnhideRatingsListener, 1000);
