227f7f59b5856c2617fb69bcab2ce5024398d927
stefan.koegel <stefan.koegel@uni-ulm.de>
2019-12-01 13:54:33 +0100

    better way to hide ranks; also hide ranks on profile page

--- hide_ogs_ranks.js
+++ hide_ogs_ranks.js
@@ -1,20 +1,24 @@
 
 // We hide most of the ratings via CSS.
-// This also includes the rating table and graph on the user pages.
-const cssStyle = `
-  span.Player-rank { display: none }
-  span.rating      { display: none }
-  span.rank        { display: none }
-  div.ratings-container       { filter: blur(20px) }
-  div.ratings-graph           { filter: blur(20px) }
-  .avatar-container span.Player-rank { filter: blur(10px); display: inline }`
+// This also includes the rating table and graph on the player pages.
+// Some ratings are added later on to the web page or are contained in player names.
+// We have to handle these differently.
+const cssStyle = `span.Player-rank { visibility: hidden }
+                  span.rating      { display: none }
+                  span.rank        { display: none }
+                
+                  div.ratings-row > div { visibility: hidden }
+                
+                  div.ratings-container > div { visibility: hidden }`;
 
-const styleElement = document.createElement("style")
-styleElement.type = "text/css"
+const styleElement = document.createElement("style");
+styleElement.type = "text/css";
 styleElement.appendChild(document.createTextNode(cssStyle));
 document.head.append(styleElement);
 
-// This function removes ranks from user names via regular expressions.
+// Hide ranks in player names that are on the website when the extension is executed.
+hideRanks(document.body);
+
 function hideRanks(node) {
   const names = node.querySelectorAll("span.player-name");
   for(const name of names) {
@@ -25,10 +29,7 @@ function hideRanks(node) {
   }
 }
 
-// Hide ranks in user names that are on the website when the extension is executed
-hideRanks(document.body);
-
-// We use a mutation observer to listen for the addition of new user names after the website has loaded
+// We use a mutation observer to listen for the addition of new player names after the website has loaded.
 const config = { childList: true, subtree: true, characterData: false, attributes: false };
 const spanObserver = new MutationObserver(hideRanksOnMutation);
 spanObserver.observe(document.body, config);
@@ -41,29 +42,31 @@ function hideRanksOnMutation(mutations) {
   }
 }
 
-// Try to add a click listener to the blurred graph and table on the user page.
-// If a click is registered, remove the blur.
+// Every second: try to add a click listener to the hidden graph and table on the player page.
+// If a click is registered, make them visible.
+// We add the 'Card' class to the containers of the hidden elements so that they look nicer.
+// We also use this class as a marker that we have already added a click listener.
+window.setInterval(addUnhideRatingsListener, 1000);
+
 function addUnhideRatingsListener() {
   const tableContainer = document.querySelector("div.ratings-container");
-  const graphContainer = document.querySelector("div.ratings-graph");
-  const playerRank = document.querySelector(".avatar-container span.Player-rank");
+  const graphContainer = document.querySelector("div.ratings-row");
 
-  if(tableContainer && graphContainer && playerRank) {
-    tableContainer.onclick = cleanUp;
-    graphContainer.onclick = cleanUp;
+  if(tableContainer && !tableContainer.classList.contains("Card")) {
+    tableContainer.onmousedown = cleanUp;
+    tableContainer.classList.add("Card");
+    graphContainer.onmousedown = cleanUp;
+    graphContainer.classList.add("Card");
 
     function cleanUp(event) {
-      tableContainer.style.setProperty("filter", "none");
-      graphContainer.style.setProperty("filter", "none");
-      playerRank.style.setProperty("filter", "none");
+      const table = document.querySelector("div.ratings-container > div");
+      const graph = document.querySelector("div.ratings-row > div");
+      const rank = document.querySelector(".avatar-container .Player-rank");
+      table.style.setProperty("visibility", "visible");
+      graph.style.setProperty("visibility", "visible");
+      rank.style.setProperty("visibility", "visible");
 
       event.stopPropagation();
-      tableContainer.onclick = null;
-      graphContainer.onclick = null;
-      window.setTimeout(addUnhideRatingsListener, 1000);
     }
-  } else {
-      window.setTimeout(addUnhideRatingsListener, 1000);
   }
 }
-window.setTimeout(addUnhideRatingsListener, 1000);
